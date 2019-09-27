94262d33e5ff5c1cd6cdeb6e2b410de5d060bd44
LeeRhodes <leerhodes@protonmail.com>
2019-09-27 21:22:23 +0100

    added basic setup

--- src/index.js
+++ src/index.js
@@ -31,7 +31,7 @@ const driver = neo4j.driver(
   process.env.NEO4J_URI || "bolt://localhost:7687",
   neo4j.auth.basic(
     process.env.NEO4J_USER || "neo4j",
-    process.env.NEO4J_PASSWORD || "neo4j"
+    process.env.NEO4J_PASSWORD || "6397854ea6"
   )
 );
 
